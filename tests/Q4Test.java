e2bdaa4fc714ede32cd215383c4f00e6694fb11d
Emily Bowe <ebowe@tcd.ie>
2020-07-19 12:06:24 +0100

    Q4 - design an algorithim to figure out if someone has won a game of tic-tac-toe

--- tests/Q4Test.java
+++ tests/Q4Test.java
@@ -5,9 +5,15 @@ class Q4Test {
 
     @Test
     public void findWinnerTest() {
-
+        /*
+        Notes on previous solutions:
+        -Incorrect use of Optionals
+            (don't pass as parameter, should be more return values, also should be more to handle an outside function)
+        -Less Global Variables
+        -Only need entry point to be public, private is default
+        -Incorrect while loop
+        -Can work with ints etc where this is more clear, e.g. int x = 1, int o = 2, int empty = 0
+         */
     }
-
-
 
 }
