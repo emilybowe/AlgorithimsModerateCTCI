831513806d3f8ed2e7effb4fa2dec611a579ef02
Emily Bowe <ebowe@tcd.ie>
2020-07-19 11:11:49 +0100

    Q4 - design an algorithim to figure out if someone has won a game of tic-tac-toe

--- src/main/java/Q4.java
+++ src/main/java/Q4.java
@@ -1,41 +1,6 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Optional;
+
 
 public class Q4 {
-    Deque<Optional> ticTacToeDeque = new ArrayDeque<>();
-
-    Optional a = ticTacToeDeque.pop();
-    Optional b = ticTacToeDeque.pop();
-    Optional c = ticTacToeDeque.pop();
-
-    public String findWinner(Optional[] resultsArray) {
-        populateDeque(resultsArray);
-        return checkForWinner();
-    }
-
-    public void populateDeque(Optional[] resultsArray) {
-        for (Optional letter : resultsArray) {
-            ticTacToeDeque.add(letter);
-        }
-    }
-
-    public String checkForWinner() {
-        {
-            if (a.equals(b) && b.equals(c)) {
-                return ("The winner is: " + a);
-            } else {
-                popNextThree();
-            }
-        }while (!ticTacToeDeque.isEmpty());
-        return ("No one won");
-    }
-
-    public void popNextThree() {
-        Optional a = ticTacToeDeque.pop();
-        Optional b = ticTacToeDeque.pop();
-        Optional c = ticTacToeDeque.pop();
-    }
 }
 
 
--- tests/Q4Test.java
+++ tests/Q4Test.java
@@ -1,24 +1,11 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import java.util.Optional;
 
 
 class Q4Test {
 
     @Test
     public void findWinnerTest() {
-        //given
-        Optional<Character> e = Optional.empty();
-        Optional<Character> o = Optional.of('o');
-        Optional<Character> x = Optional.of('x');
-        Optional[] resultsArray = {o, x, x, x, x, o, e, x, o, e, x, x, e, e, e, x, x, x, e, e, e, e, e, e};
 
-        //when
-        Q4 q4 = new Q4();
-
-        //then
-        Assertions.assertEquals("The winner is: x", q4.findWinner(resultsArray));
     }
 
 
